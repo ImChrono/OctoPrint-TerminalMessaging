514587e28548010bc4a8923d0f7aa9679f8de15d
ImChrono <golocri90@gmail.com>
2020-10-03 16:26:35 +0200

    Better customization

--- octoprint_terminalmessaging/static/js/terminalmessaging.js
+++ octoprint_terminalmessaging/static/js/terminalmessaging.js
@@ -9,11 +9,12 @@ $(function() {
         var self = this;
 
         self.terminalViewModel = parameters[0];
-
-        // Add a tag we can style to the gcode terminal output.
+         // Add a tag we can style to the gcode terminal output.
         self.terminalViewModel.log.subscribe(function(){
-            $('#terminal-output span:contains("Recv:")').addClass('received').text(function(){return $(this).text().replace('Recv: ','');});
-            $('#terminal-output span:contains("Send:")').addClass('sent').text(function(){return $(this).text().replace('Send: ','');});
+            //from plugin setting
+            //
+            $('#terminal-output span:contains("Recv:")').addClass('received small').text(function(){return $(this).text().replace('Recv: ','');});
+            $('#terminal-output span:contains("Send:")').addClass('sent small').text(function(){return $(this).text().replace('Send: ','');});
         })
     }
 
